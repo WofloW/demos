ae45b1af38727b14cf39b1bf85be6d5abb1fd3c8
Sophie Engle <sjengle@cs.usfca.edu>
2014-02-10 14:04:59 -0800

    Updated comments.

--- Classes and Objects/src/StaticDemo.java
+++ Classes and Objects/src/StaticDemo.java
@@ -7,23 +7,31 @@
  * Note that this class is designed to illustrate a specific concept, and
  * may not be an example of proper class design outside of this context.
  * </em></p>
- *
- * @see PrefixMap
- * @see PrefixDemo
  */
 public class StaticDemo {
 
-	private int a;
-	private static int b;
+	private int a;			// instance member, specific to an object
+	private static int b;	// class member, shared by all objects
 
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
+
+		/*
+		 * Lets create a StaticDemo object. Remember, a class is like a blue-
+		 * print. An object is a specific element, such as a specific house,
+		 * built from that blueprint.
+		 */
 
 		StaticDemo d1 = new StaticDemo();
 		d1.a = 0;
 		d1.b = 1;
 
 		System.out.println("d1.a = " + d1.a + ", d1.b = " + d1.b);
+
+		/*
+		 * Ignore for the moment that we did not properly initialize our
+		 * members.
+		 */
 
 		StaticDemo d2 = new StaticDemo();
 		d2.a = 2;
@@ -32,7 +40,13 @@ public class StaticDemo {
 		System.out.println("d2.a = " + d2.a + ", d2.b = " + d2.b);
 		System.out.println();
 
-		// Since b is static, we also changed the value of d1.b!
+		/*
+		 * Suppose we create another StaticDemo object. The INSTANCE member
+		 * (non-static) is specific to an object, so "d2" will have its own
+		 * value for "a". However, CLASS members are shared by all objects
+		 * (and exist even when there are no active objects), so "d2" will
+		 * share the value of "b" with "d1".
+		 */
 
 		System.out.println("d1.a = " + d1.a + ", d2.b = " + d1.b);
 		System.out.println();
